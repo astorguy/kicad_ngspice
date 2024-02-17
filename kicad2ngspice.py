f7e0ada694a10bdf2e5cce1c56164ddc691d929b
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2024-02-17 20:21:14 +0000

    fixed subsheets. all examples now working

--- kicad2ngspice.py
+++ kicad2ngspice.py
@@ -11,7 +11,12 @@ CONFIG_FILENAME = Path("/workspaces/kicad_ngspice/ki2ng.toml")
 
 # which schematic to netlist, settings must be present in config toml file
 # WHICH_SCH = "default"
+# WHICH_SCH = "laser_driver"
+# WHICH_SCH = "pspice"
+# WHICH_SCH = "rectifier"
+# WHICH_SCH = "sallen_key"
 WHICH_SCH = "subsheets"
+# WHICH_SCH = "v_i_sources"
 
 # read in the config file, toml format
 with open(CONFIG_FILENAME, "rb") as file:
